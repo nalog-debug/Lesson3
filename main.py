60a50c84dc09c86ddb5df406814f4ec1627769ff
nalog-debug <miha_s_fc_98@mail.ru>
2024-08-25 00:48:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+Number_of_completed_HA = 12
+Number_of_hours_spent = 1.5
+Course_name = "Python"
+Time_for_one_task = (Number_of_hours_spent / Number_of_completed_HA)
+print("Курс:", Course_name + ",", "всего задач:" + str(Number_of_completed_HA) + ",",
+       "затрачено часов:" + str(Number_of_hours_spent) + ",", "среднее время выполнения",
+      Time_for_one_task, "часа.")
